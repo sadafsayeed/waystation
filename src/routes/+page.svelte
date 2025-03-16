65499aae1b178db78de62710938f4a5b94804ad2
Aaron Brethorst <aaron@brethorsting.com>
2025-03-16 14:00:26 -0700

    Improve home page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,25 +29,33 @@
     });
 </script>
 
-<h1>Waystation</h1>
-
-{#if arrivalsAndDepartures.length > 0}
-    <h2>Arrivals and Departures: {arrivalsAndDepartures.length}</h2>
-    <div class='flex flex-col gap-y-2'>
-        {#each arrivalsAndDepartures as dep}
-            <div class='flex bg-red-100 gap-x-4 px-2'>
-                <div>
-                    <h2>{dep.routeShortName}</h2>
-                </div>
-                <div class='flex-1'>
-                    {dep.tripHeadsign}
-                </div>
-                <div>
-                    {getArrivalStatus(dep.predictedDepartureTime, dep.scheduledDepartureTime)}min
-                </div>
-            </div>
-        {/each}
+<div class='bg-red-100 h-screen flex flex-col'>
+    <div class='flex gap-x-4 mb-4 bg-slate-50 p-2'>
+        <h1 class='text-4xl'>Waystation</h1>
+        <h2 class='text-2xl flex-1 self-center'>Departures</h2>
+        <div class='self-center'>
+            current time here
+        </div>
     </div>
-{:else}
-    <p>Loading...</p>
-{/if}
+
+
+    {#if arrivalsAndDepartures.length > 0}
+        <div class='flex flex-col gap-y-2'>
+            {#each arrivalsAndDepartures as dep}
+                <div class='flex bg-red-100 gap-x-4 px-2'>
+                    <div>
+                        <h2 class='text-5xl'>{dep.routeShortName}</h2>
+                    </div>
+                    <div class='flex-1 text-xl self-center'>
+                        {dep.tripHeadsign}
+                    </div>
+                    <div class='text-2xl self-center'>
+                        {getArrivalStatus(dep.predictedDepartureTime, dep.scheduledDepartureTime)}min
+                    </div>
+                </div>
+            {/each}
+        </div>
+    {:else}
+        <p>Loading...</p>
+    {/if}
+</div>
