f869d77a63b3073c3fe4a15a71df72bd67a138db
Aaron Brethorst <aaron@brethorsting.com>
2025-03-18 21:28:58 -0700

    Update footer content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -177,7 +177,7 @@
 	<div class="bg-gray-300 p-3 text-black">
 		<div class="flex items-center justify-between">
 			<div class="flex items-center">
-				<span class="ml-2 text-sm">[Code:{stopCode}]</span>
+				<span class="ml-2 text-sm">Waystation by Open Transit Software Foundation</span>
 			</div>
 			<div class="text-sm">
 				Stop No. {stopCode} - {stopName}
