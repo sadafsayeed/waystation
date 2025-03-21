358c7bcbaa45407a2e8a0b825024571d483e9e8c
Sundaresan-Karunakaran <163396662+Sundaresan-Karunakaran@users.noreply.github.com>
2025-03-21 16:37:52 +0100

    Make favicon and tab title dynamic using environment variables (#21)
    Even though the favicon and tab name is kinda useless in a fullscreen mode in which this project is going to be used in, it is kinda nice to have it sometime and not see 'localhost' in the tab name

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,14 @@
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
 	let { children } = $props();
+	import { PUBLIC_OBA_LOGO_URL, PUBLIC_OBA_REGION_NAME } from '$env/static/public';
 </script>
+
+<!-- Dynamically add custom favicon -->
+<svelte:head>
+	<link rel="icon" type="image/png" href={PUBLIC_OBA_LOGO_URL} />
+	<title>{PUBLIC_OBA_REGION_NAME}</title>
+</svelte:head>
 
 <ParaglideJS {i18n}>
 	{@render children()}
