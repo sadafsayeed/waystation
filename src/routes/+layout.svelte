f96d5e02c70d3425b8d887c38520dc06249a0387
tarunsinghofficial <tarunsinghwap7@gmail.com>
2025-03-21 22:28:17 +0530

    Update viewport meta tag for large display optimization

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 <svelte:head>
 	<link rel="icon" type="image/png" href={PUBLIC_OBA_LOGO_URL} />
 	<title>{PUBLIC_OBA_REGION_NAME}</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 </svelte:head>
 
 <ParaglideJS {i18n}>
